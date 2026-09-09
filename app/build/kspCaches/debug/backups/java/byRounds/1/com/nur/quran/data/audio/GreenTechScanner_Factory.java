5ad74430fb4d014b6f83a48500ec73111e89a742
iredox <idreesadam200@gmail.com>
2026-09-09 17:21:40 +0100

    update build artifacts for audio auto-download and linked playback

---
+++ app/build/kspCaches/debug/backups/java/byRounds/1/com/nur/quran/data/audio/GreenTechScanner_Factory.java
@@ -0,0 +1,43 @@
+package com.nur.quran.data.audio;
+
+import android.content.Context;
+import dagger.internal.DaggerGenerated;
+import dagger.internal.Factory;
+import dagger.internal.QualifierMetadata;
+import dagger.internal.ScopeMetadata;
+import javax.annotation.processing.Generated;
+import javax.inject.Provider;
+
+@ScopeMetadata("javax.inject.Singleton")
+@QualifierMetadata("dagger.hilt.android.qualifiers.ApplicationContext")
+@DaggerGenerated
+@Generated(
+    value = "dagger.internal.codegen.ComponentProcessor",
+    comments = "https://dagger.dev"
+)
+@SuppressWarnings({
+    "unchecked",
+    "rawtypes",
+    "KotlinInternal",
+    "KotlinInternalInJava"
+})
+public final class GreenTechScanner_Factory implements Factory<GreenTechScanner> {
+  private final Provider<Context> contextProvider;
+
+  public GreenTechScanner_Factory(Provider<Context> contextProvider) {
+    this.contextProvider = contextProvider;
+  }
+
+  @Override
+  public GreenTechScanner get() {
+    return newInstance(contextProvider.get());
+  }
+
+  public static GreenTechScanner_Factory create(Provider<Context> contextProvider) {
+    return new GreenTechScanner_Factory(contextProvider);
+  }
+
+  public static GreenTechScanner newInstance(Context context) {
+    return new GreenTechScanner(context);
+  }
+}

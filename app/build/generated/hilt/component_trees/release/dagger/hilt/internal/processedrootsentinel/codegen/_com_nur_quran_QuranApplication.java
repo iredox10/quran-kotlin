4d77df4e685764b8d4532b35619533894c0b5da5
iredox <idreesadam200@gmail.com>
2026-09-10 12:37:35 +0100

    chore: stop tracking build outputs, add .gitignore

--- app/build/generated/hilt/component_trees/release/dagger/hilt/internal/processedrootsentinel/codegen/_com_nur_quran_QuranApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.nur.quran.QuranApplication"
-)
-public final class _com_nur_quran_QuranApplication {
-}
